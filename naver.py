668e2f63fb4c7124ebf299564d7814d1c47420d6
unknown <hsk3514@gmail.com>
2019-05-07 20:57:17 +0900

    네이버 실시간 검색어 20위 까지

--- naver.py
+++ naver.py
@@ -2,4 +2,8 @@ import requests
 from bs4 import BeautifulSoup
 
 req = requests.get("https://www.naver.com/")
-print(req.text)
+soup = BeautifulSoup(req.text, 'html.parser')
+titles = soup.find_all('span',{'class':'ah_k'})
+
+for x in titles:
+    print(x.get_text())
