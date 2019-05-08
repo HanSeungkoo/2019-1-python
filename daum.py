4ab238f44d489229a0859b8eaa539fcd428d8d54
unknown <hsk3514@gmail.com>
2019-05-08 20:33:51 +0900

    5월8일스터디_Select

--- daum.py
+++ daum.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 
 req = requests.get("https://www.daum.net/")
 soup = BeautifulSoup(req.text, 'html.parser')
-titles = soup.find_all('span',{'class':'txt_issue'})
+titles = soup.select('div.roll_txt').select('div.rank_count').select('span.txt_issue').select('a.link_issue')[0]
 
-for x in titles:
+for x in ex_list:
     print(x.text)
